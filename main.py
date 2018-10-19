3c17de8a98143bad7db1701e52602c94450da4be
vinhub <vin@introp.net>
2018-10-19 15:07:42 -0700

    param tweaking and bug fixes

--- main.py
+++ main.py
@@ -33,18 +33,18 @@ def toonifyImage(inputImage, blurriness, thickness):
     smoothImage = proc.SmoothImages(inputImage, 7, blurriness)
     edgeMask = proc.GetEdgeMask(smoothImage)
     dilatedEdges = proc.EdgesDilation(edgeMask, thickness)
-    toonifiedImage = proc.Toonify(smoothImage, dilatedEdges)
+    outputImage = proc.Toonify(smoothImage, dilatedEdges)
     
-    return toonifiedImage
+    return outputImage
     
 
 def sketchifyImage(inputImage, kSize, darkness):
     grayScaleImage = proc.getGrayScale(inputImage)
     negativeImage = proc.getNegative(grayScaleImage)
     sketchMask = proc.getSketchMask(negativeImage, kSize)
-    sketchifiedImage = proc.Sketchify(grayScaleImage, sketchMask, darkness)
+    outputImage = proc.Sketchify(grayScaleImage, sketchMask, darkness)
 
-    return sketchifiedImage
+    return outputImage
     
     
 if __name__ == "__main__":
@@ -67,54 +67,60 @@ if __name__ == "__main__":
 
     blurriness = 15
     thickness = 4
-    kSize = 11
-    darkness = 298 # 258
+    kSize = 151 # 11
+    darkness = 300 # 258
+
+    image = cv2.imread(imageFile, cv2.IMREAD_UNCHANGED | cv2.IMREAD_COLOR)
 
     def updateBlurriness(value):
         blurriness = value
-        toonifiedImage = toonifyImage(image, blurriness, thickness)
-        cv2.imshow('toon', toonifiedImage)
+        toonified = toonifyImage(image, blurriness, thickness)
+        cv2.imshow('toon', toonified)
 
     def updateThickness(value):
         thickness = value
-        toonifiedImage = toonifyImage(image, blurriness, thickness)
-        cv2.imshow('toon', toonifiedImage)
+        toonified = toonifyImage(image, blurriness, thickness)
+        cv2.imshow('toon', toonified)
 
     def updateKSize(value):
         kSize = value
         if (kSize % 2 == 0):
             kSize += 1 # must be odd
-        sketchifiedImage = sketchifyImage(image, kSize, darkness)
-        cv2.imshow('sketch', sketchifiedImage)
+        sketchified = sketchifyImage(image, kSize, darkness)
+        cv2.imshow('sketch', sketchified)
 
     def updateDarkness(value):
         darkness = value
-        sketchifiedImage = sketchifyImage(image, kSize, darkness)
-        cv2.imshow('sketch', sketchifiedImage)
+        sketchified = sketchifyImage(image, kSize, darkness)
+        cv2.imshow('sketch', sketchified)
 
-    image = cv2.imread(imageFile, cv2.IMREAD_UNCHANGED | cv2.IMREAD_COLOR)
-    
     if type == 'toon':
         cv2.namedWindow('toon', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('toon', 1600, 800)
+        cv2.resizeWindow('toon', 1800, 900)
         cv2.createTrackbar('blurriness', 'toon', blurriness, 30, updateBlurriness)
         cv2.createTrackbar('thickness', 'toon', thickness, 10, updateThickness)
         cv2.imshow('toon', image)
+        updateBlurriness(blurriness) # initialize
     else:
         cv2.namedWindow('sketch', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('sketch', 1600, 800)
-        cv2.createTrackbar('kSize', 'sketch', kSize, 15, updateKSize)
+        cv2.resizeWindow('sketch', 1800, 900)
+        cv2.createTrackbar('kSize', 'sketch', kSize, 200, updateKSize)
         cv2.createTrackbar('darkness', 'sketch', darkness, 300, updateDarkness)
         cv2.imshow('sketch', image)
+        updateKSize(kSize) # initialize
 
-    # if (type == 'toon'):
-        # toonifiedFile = os.path.join(os.path.dirname(imageFile), 'toon_' + os.path.basename(imageFile))
-        # cv2.imwrite(toonifiedFile, toonifiedImage)
-        # subprocess.check_call(["exiftool.exe", "-ProjectionType=equirectangular", toonifiedFile])
-    # else:
-        # sketchifiedFile = os.path.join(os.path.dirname(imageFile), 'sketch_' + os.path.basename(imageFile))
-        # cv2.imwrite(sketchifiedFile, sketchifiedImage)
-        # subprocess.check_call(["exiftool.exe", "-ProjectionType=equirectangular", sketchifiedFile])
+    key = cv2.waitKey(0)      
+    
+    if (key == ord('s')):
+      if (type == 'toon'):
+          toonifiedFile = os.path.join(os.path.dirname(imageFile), 'toon_' + os.path.basename(imageFile))
+          toonifiedImage = toonifyImage(image, blurriness, thickness)
+          cv2.imwrite(toonifiedFile, toonifiedImage)
+          subprocess.check_call(["exiftool.exe", "-ProjectionType=equirectangular", toonifiedFile])
+      else:
+          sketchifiedFile = os.path.join(os.path.dirname(imageFile), 'sketch_' + os.path.basename(imageFile))
+          sketchifiedImage = sketchifyImage(image, kSize, darkness)
+          cv2.imwrite(sketchifiedFile, sketchifiedImage)
+          subprocess.check_call(["exiftool.exe", "-ProjectionType=equirectangular", sketchifiedFile])
       
-    cv2.waitKey(0)      
     cv2.destroyAllWindows()
